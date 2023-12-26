3949bdfd1bad40d86a4337520b96b4e1f593e676
Pawel Rynowiecki <pawel.rynowiecki@anixe.pl>
2023-12-26 19:57:33 +0100

    Fix first info message

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::from_args();
-    info!("Starting with args: {:?}", args);
-    let config = Config::from(args);
     let subscriber = FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
+    let args = Args::from_args();
+
+    info!("Starting with args: {:?}", args);
+
+    let config = Config::from(args);
 
     match config.network {
         Network::Bitcoin => run_bitcoin_handshake(config).await?,
